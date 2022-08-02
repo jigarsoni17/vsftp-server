3aea47bf32bbdf67f24ee61b00b06ec71a67c174
jigarsoni17 <51534139+jigarsoni17@users.noreply.github.com>
2022-08-02 08:18:23 +0530

    Add files via upload

--- vsftp.ubuntu.sh
+++ vsftp.ubuntu.sh
@@ -1,42 +1,42 @@
-#!/bin/bash
-
-#install vsftp service 
-echo -e "Installing vsftp service into your machine....."
-
-sudo apt install httpd -y 
-
-sed -i 's/anonymous_enable=YES/anonymous_enable=NO/' vsftp.conf && sed -i 's/local_enable=NO/local_enable=YES/' vsftp.conf && sed -i 's/write_enable=NO/write_enable=YES/' vsftp.conf && sed -i 's/chroot_local_user=NO/chroot_local_user=YES/' vsftp.conf | sudo tee -a /etc/vsftpd.conf 
-
-#add user for vsftp service 
-echo -e " Add a user for vsftp...."
-
-if [ $(id -u) -eq 0 ]; then
-	read -p "Enter username : " username
-	read -s -p "Enter password : " password
-	egrep "^$username" /etc/passwd >/dev/null
-	if [ $? -eq 0 ]; then
-		echo "$username exists!"
-		exit 1
-	else
-		pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
-		useradd -m -p "$pass" "$username"
-		[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
-	fi
-else
-	echo "Only root may add a user to the system."
-	exit 2
-fi
-
-#mkdir /etc/ssl/private
-openssl req -x509 -nodes -days 3650 -newkey rsa:1024 -keyout /etc/ssl/private/vsftpd.pem -out /etc/ssl/private/vsftpd.pem
-
-ip=$(hostname -I | awk '{print $1}')
-
-echo -e "rsa_cert_file=/etc/ssl/private/vsftpd.pem\nrsa_private_key_file=/etc/ssl/private/vsftpd.pem\nssl_enable=YES\nallow_anon_ssl=NO\nforce_local_data_ssl=YES\nforce_local_logins_ssl=YES\nssl_tlsv1=YES\nssl_sslv2=NO\nssl_sslv3=NO\nrequire_ssl_reuse=NO\nssl_ciphers=HIGH" >> vsftp.conf && echo -e "# BEGIN ANSIBLE MANAGED BLOCK\nchroot_local_user=YES\nchroot_list_enable=YES\nchroot_list_file=/etc/vsftpd/chroot_list\n#port_enable=YES\n#pasv_enable=YES\npasv_promiscuous=YES\npasv_max_port=24200\npasv_min_port=24000\npasv_address=$ip" >> vsftp.conf | sudo tee -a /etc/vsftpd.conf
-n
-mkdir -p /etc/vsftpd/chroot_list 
-echo -e "ftpuser" | sudo tee -a /etc/vsftpd/chroot_list
-
-systemctl enable --now vsftpd.service
-
-usermod -d /var/www ftpuser
+rsa_cert_file=/etc/ssl/private/vsftpd.pem
+rsa_private_key_file=/etc/ssl/private/vsftpd.pem
+ssl_enable=YES
+allow_anon_ssl=NO
+force_local_data_ssl=YES
+force_local_logins_ssl=YES
+ssl_tlsv1=YES
+ssl_sslv2=NO
+ssl_sslv3=NO
+require_ssl_reuse=NO
+ssl_ciphers=HIGH
+# BEGIN ANSIBLE MANAGED BLOCK
+chroot_local_user=YES
+chroot_list_enable=YES
+chroot_list_file=/etc/vsftpd/chroot_list
+#port_enable=YES
+#pasv_enable=YES
+pasv_promiscuous=YES
+pasv_max_port=24200
+pasv_min_port=24000
+pasv_address=
+rsa_cert_file=/etc/ssl/private/vsftpd.pem
+rsa_private_key_file=/etc/ssl/private/vsftpd.pem
+ssl_enable=YES
+allow_anon_ssl=NO
+force_local_data_ssl=YES
+force_local_logins_ssl=YES
+ssl_tlsv1=YES
+ssl_sslv2=NO
+ssl_sslv3=NO
+require_ssl_reuse=NO
+ssl_ciphers=HIGH
+# BEGIN ANSIBLE MANAGED BLOCK
+chroot_local_user=YES
+chroot_list_enable=YES
+chroot_list_file=/etc/vsftpd/chroot_list
+#port_enable=YES
+#pasv_enable=YES
+pasv_promiscuous=YES
+pasv_max_port=24200
+pasv_min_port=24000
+pasv_address=
